5dd56a669f6c88fff5bcbcc4dfe021caaefe2821
bmteasdale <bmteasdale3@gmail.com>
2021-03-17 10:56:33 -0300

    adds MongoDB Connection

--- src/main/java/ca/csci483/myprojectname/model/DBConnection.java
+++ src/main/java/ca/csci483/myprojectname/model/DBConnection.java
@@ -9,25 +9,43 @@ package ca.csci483.myprojectname.model;
  *
  * @author bmteasdale
  */
+import com.mongodb.*;
+import io.github.cdimascio.dotenv.Dotenv;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
 public class DBConnection {
-    private final String HOST;
-    private final String PORT;
-    private final String DB_NAME;
-    private final String USER_NAME;
-    private final String PASSWORD;
-    
+    private String dbURI;
+    private String dbName;
+    private MongoClient client;
+    private DB database;
+    private DBCollection userCollection;
+    private DBCollection meetingCollection;
     
     public DBConnection(){
-        this.HOST = "localhost";
-        this.PORT = "3306";
-        this.DB_NAME = "meetme";
-        this.USER_NAME = "csci483";
-        this.PASSWORD = "csci483";
+        Dotenv dotenv = Dotenv.configure()
+        .directory("../utils")
+        .ignoreIfMalformed()
+        .ignoreIfMissing()
+        .load();
+       
+        this.dbURI = dotenv.get("DB_URI");
+        this.dbName = dotenv.get("DB_NAME");
+        try {
+            MongoClientURI uri = new MongoClientURI(this.dbURI);
+            MongoClient mongoClient = new MongoClient(uri);
+            this.database = mongoClient.getDB(this.dbName);
+            this.userCollection = database.getCollection("user");
+            this.meetingCollection = database.getCollection("meeting");
+            System.out.println("Mongo successfully connected!");
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
     };
     
 }
